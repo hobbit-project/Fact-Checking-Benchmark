b86a03a8c9793f90aec1f599b67349a3eec5fa65
Zafar Syed <zafar.habeeb1989@gmail.com>
2019-06-14 11:17:28 +0200

    Added data generation logic from file - SWC2019

--- src/main/java/org/dice/factcheckbenchmark/benchmark/DataGenerator.java
+++ src/main/java/org/dice/factcheckbenchmark/benchmark/DataGenerator.java
@@ -1,5 +1,8 @@
 package org.dice.factcheckbenchmark.benchmark;
 
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 import org.dice.factcheckbenchmark.BenchmarkConstants;
 import org.hobbit.core.components.AbstractDataGenerator;
 import org.slf4j.Logger;
@@ -7,9 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.dice.factcheckbenchmark.benchmark.rdf.TripleExtractor;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Random;
 
 public class DataGenerator extends AbstractDataGenerator {
     private static final Logger logger = LoggerFactory.getLogger(DataGenerator.class);
@@ -65,6 +73,80 @@ public class DataGenerator extends AbstractDataGenerator {
         } else {
             logger.debug("No files found in {}", factbenchPath);
         }
+    }
+
+    private void sendDataFromFile(final Path factbenchPath) throws IOException {
+
+        if (Files.isDirectory(factbenchPath)) {
+            Files.walkFileTree(factbenchPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path filePath, BasicFileAttributes attrs) throws IOException {
+
+                    if (filePath.toString().endsWith("nt")) {
+
+                        Model model = ModelFactory.createDefaultModel();
+                        model.read(new FileInputStream(new File(filePath.toString())), null, "N-TRIPLES");
+                        ArrayList<String> list = new ArrayList<String>();
+                        StmtIterator stmtIterator = model.listStatements(null, RDF.type, RDF.Statement);
+                        while (stmtIterator.hasNext()){
+                            String s = stmtIterator.nextStatement().getSubject().toString();
+                            list.add(s);
+                        }
+
+                        Collections.sort(list);
+                        Collections.shuffle(list, new Random(5));
+
+                        Iterator<String> listIterator = list.iterator();
+
+                        while(listIterator.hasNext()){
+                            String statement = listIterator.next();
+                            String data = returnStatementGraph(ResourceFactory.createResource(statement), model);
+                            sendDataToTaskGenerator(data.getBytes());
+                        }
+
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+        else {
+            logger.debug("No files found in {}", factbenchPath);
+        }
+    }
+
+    public String returnStatementGraph(Resource statement, Model model){
+
+        Statement subject = model.listStatements(statement, RDF.subject, (RDFNode)null).nextStatement();
+        Statement object = model.listStatements(statement, RDF.object, (RDFNode)null).nextStatement();
+        Statement property = model.listStatements(statement, RDF.predicate, (RDFNode)null).nextStatement();
+
+        Statement subjectLabel = model.listStatements(subject.getObject().asResource(), RDFS.label, (RDFNode)null).nextStatement();
+        Statement objectLabel = model.listStatements(object.getObject().asResource(), RDFS.label, (RDFNode)null).nextStatement();
+
+        Statement truthValue = model.listStatements(statement, ResourceFactory.createProperty("http://swc2017.aksw.org/hasTruthValue"), (RDFNode)null).nextStatement();
+
+        String truth = "";
+
+        if(truthValue.getObject().asLiteral().getDouble()==1.0)
+            truth = "/correct/";
+        else
+            truth = "/wrong/";
+
+        String data = String.format("%s:*:"+"<%s> " + "<%s> " + "<%s> " +".\n"+
+                        "<%s> " + "<%s> " + "<%s> " +".\n"+
+                        "<%s> " + "<%s> " + "<%s> " +".\n"+
+                        "<%s> " + "<%s> " + "<%s> " +".\n"+
+                        "<%s> " + "<%s> " + "\"%s\"@en " +".\n"+
+                        "<%s> " + "<%s> " + "\"%s\"@en " +".",
+                truth, statement.toString(), RDF.type.toString(), RDF.Statement.toString(),
+                subject.getSubject(), subject.getPredicate(), subject.getObject(),
+                object.getSubject(), object.getPredicate(), object.getObject(),
+                property.getSubject(), property.getPredicate(), property.getObject(),
+                subjectLabel.getSubject(), subjectLabel.getPredicate(), subjectLabel.getObject(),
+                objectLabel.getSubject(), objectLabel.getPredicate(), objectLabel.getObject());
+
+
+        return data;
     }
 
     private String setFileIdentifier(String rootDir, String directory) {
